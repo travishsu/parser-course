95820e5e839ce045fe91e39593d3f83dfca8719f
Yichung Hsu <fov223@gmail.com>
2016-03-01 15:33:02 +0800

    week4

--- week3/urllib_example.py
+++ week3/urllib_example.py
@@ -1,5 +1,9 @@
 import urllib
 fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
 
+word_count = dict()
 for line in fhand:
-    print line.strip()
+    words = line.split()
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+print word_count
